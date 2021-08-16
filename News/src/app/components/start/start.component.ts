10f1f8c308e1ba1240a233fafa60cf1fe0f5e674
Leonardo Brancaglione <leonardobrancaglione@hotmail.com>
2021-08-15 23:50:10 -0300

    Versão 0.3

--- News/src/app/components/start/start.component.ts
+++ News/src/app/components/start/start.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-start',
@@ -7,8 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StartComponent implements OnInit {
 
-  constructor() { }
+  //Declaração do Router esponsavel por executar a navegação entre as paginas
+  constructor(private router: Router) { }
 
   ngOnInit() {}
+  //função que executa a navegação até a pagina login
+  navigateToLoginPage(){
+    this.router.navigate(['login'])
+  }
 
 }
